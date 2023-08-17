3b99622fa9fcc27f13325c879c23afd9274dbaba
Joshua Woods developer <nordev@protonmail.com>
2023-08-17 16:17:59 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `learning`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
